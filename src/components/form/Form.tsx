b5fffa2d60fd511d3de38e652104e88ca340c519
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-03-10 19:47:59 +0300

    Added state management and input handling to Form component

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,17 +1,47 @@
+import { ChangeEvent, useState } from 'react'
 import styles from './Form.module.scss'
 
 export const Form = () => {
+	const [name, setName] = useState<string>('')
+	const [lastName, setLastName] = useState<string>('')
+	const [status, setStatus] = useState<string>('')
+
+	const handleInputName = (e: ChangeEvent<HTMLInputElement>) => {
+		setName(e.target.value)
+	}
+	const handleInputLastName = (e: ChangeEvent<HTMLInputElement>) => {
+		setLastName(e.target.value)
+	}
+	const handleInputStatus = (e: ChangeEvent<HTMLInputElement>) => {
+		setStatus(e.target.value)
+	}
+
 	return (
 		<div className={styles.form}>
 			<div className={styles.formContainer}>
 				<div className={styles.formRow}>
 					<img className={styles.formIcon} src='/form.png' alt='' />
 					<div className={styles.inputsArea}>
-						<input type='text' placeholder='имя' />
-						<input type='text' placeholder='фамилия' />
+						<input
+							value={name}
+							onChange={handleInputName}
+							type='text'
+							placeholder='имя'
+						/>
+						<input
+							value={lastName}
+							onChange={handleInputLastName}
+							type='text'
+							placeholder='фамилия'
+						/>
 					</div>
 					<div className={styles.inputsArea}>
-						<input type='text' placeholder='должность' />	
+						<input
+							value={status}
+							onChange={handleInputStatus}
+							type='text'
+							placeholder='должность'
+						/>
 					</div>
 				</div>
 			</div>
